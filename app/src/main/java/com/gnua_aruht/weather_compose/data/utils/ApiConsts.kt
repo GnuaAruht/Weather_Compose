2a175eab5aed578ed96a94da15e8712960997e28
GnuaAruht <thuraaung6942@gmail.com>
2025-03-07 14:32:16 +0630

    remove ApiConsts from tracking

--- app/src/main/java/com/gnua_aruht/weather_compose/data/utils/ApiConsts.kt
+++
@@ -1,7 +0,0 @@
-package com.gnua_aruht.weather_compose.data.utils
-
-object ApiConsts {
-    const val BASE_URL = "https://api.openweathermap.org/data/3.0/"
-    const val API_KEY = "YOUR_API_KEY"
-    const val ONE_CALL = "onecall"
-}
